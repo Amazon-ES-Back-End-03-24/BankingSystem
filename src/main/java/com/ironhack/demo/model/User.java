e328c7029d6e099ebf0125c6fa502cbc0d4c206d
mohaessghir <essghirm5@gmail.com>
2024-06-22 16:22:09 +0200

    add security

--- src/main/java/com/ironhack/demo/model/User.java
+++ src/main/java/com/ironhack/demo/model/User.java
@@ -3,7 +3,11 @@ package com.ironhack.demo.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+
+import static jakarta.persistence.FetchType.EAGER;
 
 @Entity
 public class User {
@@ -17,32 +21,57 @@ public class User {
     @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,orphanRemoval = true)
     private List<Account> accounts;
 
+    @ManyToMany(fetch = EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public List<Account> getAccounts() {
         return accounts;
     }
+
     public void setAccounts(List<Account> accounts) {
-
         this.accounts = accounts;
-
     }
 
+    public Collection<Role> getRoles() {
+        return roles;
+    }
 
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password, List<Account> accounts, Collection<Role> roles) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.accounts = accounts;
+        this.roles = roles;
+    }
 }
